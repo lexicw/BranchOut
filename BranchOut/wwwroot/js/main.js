b4821e110cb0314de93cf30f3ddfa7bf6b959c8a
Lexi Wheeler <lwheeler@theamegroup.com>
2025-02-13 13:37:03 -0500

    Move reloadiframe function

--- BranchOut/wwwroot/js/main.js
+++ BranchOut/wwwroot/js/main.js
@@ -5,3 +5,19 @@
 function hideModal(modalId) {
     bootstrap.Modal.getOrCreateInstance(document.getElementById(modalId)).hide();
 }
+
+window.reloadIframe = function (iframeId) {
+    const iframe = document.getElementById(iframeId);
+
+    if (iframe) {
+        const parent = iframe.parentNode;
+
+        const newIframe = iframe.cloneNode();
+
+        newIframe.onload = function () {
+            parent.removeChild(iframe);
+        };
+
+        parent.insertBefore(newIframe, iframe);
+    }
+};
