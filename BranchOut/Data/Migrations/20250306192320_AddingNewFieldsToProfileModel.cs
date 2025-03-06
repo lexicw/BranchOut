d65b2b94eb9ca24b9274df957af765a754e0dc57
Lexi Wheeler <lwheeler@theamegroup.com>
2025-03-06 14:23:47 -0500

    Migrations

---
+++ BranchOut/Data/Migrations/20250306192320_AddingNewFieldsToProfileModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BranchOut.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingNewFieldsToProfileModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ButtonStyle",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Font",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ButtonStyle",
+                table: "Profile");
+
+            migrationBuilder.DropColumn(
+                name: "Font",
+                table: "Profile");
+        }
+    }
+}
