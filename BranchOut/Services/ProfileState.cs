5bbd6e420bc82c7a3258d2a36378aed3fc7da7a3
Lexi Wheeler <lwheeler@theamegroup.com>
2025-03-06 10:48:58 -0500

    Setting up profile state

---
+++ BranchOut/Services/ProfileState.cs
@@ -0,0 +1,35 @@
+﻿using BranchOut.Data;
+
+namespace BranchOut.Services
+{
+    public class ProfileState
+    {
+        public event Action OnChange;
+
+        private Profile _profile { get; set; } = new Profile();
+        private List<Link> _links { get; set; } = new();
+
+        public Profile Profile
+        {
+            get => _profile;
+            set
+            {
+                _profile = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public List<Link> Links
+        {
+            get => _links;
+            set
+            {
+                _links = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+
+}
