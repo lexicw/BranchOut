426acd8b6aa053907481c834513cc1f466941f32
Lexi Wheeler <lwheeler@theamegroup.com>
2025-03-06 14:22:43 -0500

    Add font and button style to profile

--- BranchOut/Data/Profile.cs
+++ BranchOut/Data/Profile.cs
@@ -9,5 +9,7 @@
         public string Theme { get; set; }
         public ICollection<Link> Links { get; set; } = new List<Link>();
         public ApplicationUser? ApplicationUser { get; set; }
+        public string Font { get; set; }
+        public string ButtonStyle { get; set; }
     }
 }
