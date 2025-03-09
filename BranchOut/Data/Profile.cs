06e99f7a385eccff8b7ca4b71d2ff989e9203fd5
lexicw <lexicw15@yahoo.com>
2025-03-08 22:02:02 -0500

    Add Text Color field

--- BranchOut/Data/Profile.cs
+++ BranchOut/Data/Profile.cs
@@ -10,6 +10,7 @@
         public ICollection<Link> Links { get; set; } = new List<Link>();
         public ApplicationUser? ApplicationUser { get; set; }
         public string? Font { get; set; }
+        public string? TextColor { get; set; }
         public string? ButtonStyle { get; set; }
     }
 }
