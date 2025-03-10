e2501dbbf814614a49a0eeda7fd12f0d77cdbd04
Lexi Wheeler <lwheeler@theamegroup.com>
2025-03-10 15:24:53 -0400

    Add Button styles section to Profile Settings and social media section to Links

---
+++ BranchOut/Data/Migrations/20250310172601_addButtonStyles.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BranchOut.Migrations
+{
+    /// <inheritdoc />
+    public partial class addButtonStyles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ButtonStyle",
+                table: "Profile",
+                newName: "ButtonTextColor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ButtonBorder",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ButtonColor",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ButtonShape",
+                table: "Profile",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ButtonBorder",
+                table: "Profile");
+
+            migrationBuilder.DropColumn(
+                name: "ButtonColor",
+                table: "Profile");
+
+            migrationBuilder.DropColumn(
+                name: "ButtonShape",
+                table: "Profile");
+
+            migrationBuilder.RenameColumn(
+                name: "ButtonTextColor",
+                table: "Profile",
+                newName: "ButtonStyle");
+        }
+    }
+}
