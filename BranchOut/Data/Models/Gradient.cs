f9b801719f448940089e91fea8251206b0398f60
Lexi Wheeler <lwheeler@theamegroup.com>
2025-03-31 17:11:12 -0400

    Adding CRUD functionality to QuickGrid. Playing around with the modal and making it dynamic and reusable

--- BranchOut/Data/Models/Gradient.cs
+++ BranchOut/Data/Models/Gradient.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BranchOut.Data.Models
 {
     public class Gradient
     {
         public int Id { get; set; }
+        [Required]
         public string? Value { get; set; }
         public string? CssClass { get; set; }
 
